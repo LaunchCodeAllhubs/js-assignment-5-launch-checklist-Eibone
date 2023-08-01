1decc00cb6f2979c9131ed643a1c7539ceec8a4d
William R <wrowe31@yahoo.com>
2023-07-31 19:01:36 -0500

    More autograder attunement

--- scriptHelper.js
+++ scriptHelper.js
@@ -67,23 +67,23 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
     if (fuelLevel < 10000 && cargoLevel > 10000) {
         launchStatusCheck.innerHTML = "Shuttle Not Ready for Launch";
-        launchStatusCheck.style.color = "red";
+        launchStatusCheck.style.color = "rgb(199, 37, 78)";
         fuelStatusCheck.innerHTML = "Fuel level too low for launch";
         cargoStatusCheck.innerHTML = "Cargo mass too heavy for launch";
 
         } else if (fuelLevel > 10000 && cargoLevel > 10000) {
             launchStatusCheck.innerHTML = "Shuttle Not Ready for Launch";
-            launchStatusCheck.style.color = "red";
+            launchStatusCheck.style.color = "rgb(199, 37, 78)";
             cargoStatusCheck.innerHTML = "Cargo mass too heavy for launch";
             fuelStatusCheck.innerHTML = "Fuel level high enough for launch";
 
 
             } else if (fuelLevel < 10000 && cargoLevel < 10000) {
                 launchStatusCheck.innerHTML = "Shuttle Not Ready for Launch";
-                launchStatusCheck.style.color = "red";
+                launchStatusCheck.style.color = "rgb(199, 37, 78)";
                 fuelStatusCheck.innerHTML = "Fuel level too low for launch";
                 cargoStatusCheck.innerHTML = "Cargo mass low enough for launch";
-                } else {
+                } else if (fuelLevel > 10000 && cargoLevel < 10000) {
                     launchStatusCheck.style.color = "#419F6A";
                     launchStatusCheck.innerHTML = "Shuttle is Ready for Launch";
                     fuelStatusCheck.innerHTML = "Fuel level high enough for launch";
