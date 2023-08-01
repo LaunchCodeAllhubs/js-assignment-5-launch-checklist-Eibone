212e98a6aeb678f14e6dd4b16aaec760884fc550
William R <wrowe31@yahoo.com>
2023-07-31 19:06:27 -0500

    .

--- scriptHelper.js
+++ scriptHelper.js
@@ -63,32 +63,32 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
     pilotStatusCheck.innerHTML = `Pilot ${pilot} is ready for launch`;
     copilotStatusCheck.innerHTML = `Co-pilot ${copilot} is ready for launch`;
-    faultyItemsCheck.style.visibility = "visible";
 
     if (fuelLevel < 10000 && cargoLevel > 10000) {
         launchStatusCheck.innerHTML = "Shuttle Not Ready for Launch";
         launchStatusCheck.style.color = "rgb(199, 37, 78)";
         fuelStatusCheck.innerHTML = "Fuel level too low for launch";
         cargoStatusCheck.innerHTML = "Cargo mass too heavy for launch";
-
+        faultyItemsCheck.style.visibility = "visible";
         } else if (fuelLevel > 10000 && cargoLevel > 10000) {
             launchStatusCheck.innerHTML = "Shuttle Not Ready for Launch";
             launchStatusCheck.style.color = "rgb(199, 37, 78)";
             cargoStatusCheck.innerHTML = "Cargo mass too heavy for launch";
             fuelStatusCheck.innerHTML = "Fuel level high enough for launch";
-
+            faultyItemsCheck.style.visibility = "visible";
 
             } else if (fuelLevel < 10000 && cargoLevel < 10000) {
                 launchStatusCheck.innerHTML = "Shuttle Not Ready for Launch";
                 launchStatusCheck.style.color = "rgb(199, 37, 78)";
                 fuelStatusCheck.innerHTML = "Fuel level too low for launch";
                 cargoStatusCheck.innerHTML = "Cargo mass low enough for launch";
-                } else if (fuelLevel > 10000 && cargoLevel < 10000) {
-                    launchStatusCheck.style.color = "#419F6A";
+                faultyItemsCheck.style.visibility = "visible";
+                } else {
+                    launchStatusCheck.style.color = "rgb(65, 159, 106)";
                     launchStatusCheck.innerHTML = "Shuttle is Ready for Launch";
                     fuelStatusCheck.innerHTML = "Fuel level high enough for launch";
                     cargoStatusCheck.innerHTML = "Cargo mass low enough for launch";
-
+                    faultyItemsCheck.style.visibility = "visible";
             }    
 }
 
